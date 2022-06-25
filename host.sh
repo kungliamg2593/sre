7a50afdadca5440606664e67daedf7085d6e1ac8
kungliamg2593 <107601253+kungliamg2593@users.noreply.github.com>
2022-06-25 11:32:11 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cat /etc/hosts | ssh w1 "sudo sh -c 'cat >/etc/hosts'"
 cat /etc/hosts | ssh w2 "sudo sh -c 'cat >/etc/hosts'"
+echo "m1"|sudo tee /etc/hostname
 echo "w1"|ssh w1 "sudo sh -c 'cat >/etc/hostname'"
 echo "w2"|ssh w2 "sudo sh -c 'cat >/etc/hostname'"
 ssh w1 'sudo reboot';ssh w2 'sudo reboot';sudo reboot
