dc3d5c2880eb5010e29cb3b262a01635f8372583
kungliamg2593 <107601253+kungliamg2593@users.noreply.github.com>
2024-05-08 10:31:39 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -5,13 +5,13 @@ sysctl -w net.bridge.bridge-nf-call-iptables=1
 echo "br_netfilter" |tee /etc/modules-load.d/k8s.conf
 echo "net.ipv6.conf.all.disable_ipv6 = 1" |tee /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" |tee /etc/sysctl.conf
-cat /etc/fstab | grep -v swap |sudo tee temp.fstab
-cat temp.fstab |sudo tee /etc/fstab
+cat /etc/fstab | grep -v swap |tee temp.fstab
+cat temp.fstab |tee /etc/fstab
 rm temp.fstab
 swapoff -a
 mount --make-rshared /
-echo '#!/bin/sh' |sudo tee /etc/local.d/sharemetrics.start
-echo "mount --make-rshared /" |sudo tee -a  /etc/local.d/sharemetrics.start
+echo '#!/bin/sh' |tee /etc/local.d/sharemetrics.start
+echo "mount --make-rshared /" |tee -a  /etc/local.d/sharemetrics.start
 chmod +x /etc/local.d/sharemetrics.start
 rc-update add local
 uuidgen |sudo tee /etc/machine-id
@@ -22,7 +22,7 @@ service kubelet restart
 /etc/init.d/ntpd start
 /etc/init.d/docker start
 ln -s /usr/libexec/cni/flannel-amd64 /usr/libexec/cni/flannel
-echo "net.bridge.bridge-nf-call-iptables=1" |sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables=1" |tee -a /etc/sysctl.conf
 sysctl net.bridge.bridge-nf-call-iptables=1
 rc-update add crio default
 echo '
