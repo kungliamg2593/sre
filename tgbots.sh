40a57a07d790f8ed5ff81613d2875f59f7d0b635
kungliamg2593 <107601253+kungliamg2593@users.noreply.github.com>
2024-05-23 17:10:09 +0800

    Update tgbots.sh

--- tgbots.sh
+++ tgbots.sh
@@ -1,2 +1,1 @@
-TAG_NAME=$(echo "${Tags}" | awk -F'/' '{print $NF}')
-curl -X POST "https://api.telegram.org/bot7194383189:AAEpSgHrOvALwf5BdM5er-a3resHrc8zqGc/sendMessage" -d "chat_id=-4211449241&text=${env}站${TAG_NAME}分支#${BUILD_ID}：SUCCESS"
+curl -X POST "https://api.telegram.org/bot7194383189:AAEpSgHrOvALwf5BdM5er-a3resHrc8zqGc/sendMessage" -d "chat_id=-4211449241&text=${env}站${default_branch}分支#${BUILD_ID}：SUCCESS"
