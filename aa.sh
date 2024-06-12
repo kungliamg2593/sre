d733b1114b815c7c603433dcf488124672f4cc2a
kungliamg2593 <107601253+kungliamg2593@users.noreply.github.com>
2024-06-12 10:46:57 +0800

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -1,60 +1,77 @@
 #!/bin/bash
-apk update; apk upgrade
-apk add nano
-nano /etc/apk/repositories
-http://dl-cdn.alpinelinux.org/alpine/edge/testing
-http://dl-cdn.alpinelinux.org/alpine/edge/community
-http://dl-cdn.alpinelinux.org/alpine/edge/main
 
-apk update && apk add tree unzip curl wget zip grep bash procps util-linux-misc dialog go udev sudo pciutils podman cni-plugin-flannel cni-plugins flannel flannel-contrib-cni kubectl kubelet kubeadm docker uuidgen nfs-utils cri-o cri-tools
 echo "root:root" | chpasswd
 echo 'PermitRootLogin yes' | tee -a /etc/ssh/ssh_config
 echo 'StrictHostKeyChecking no' | tee -a /etc/ssh/ssh_config
+
+swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
+
+ufw disable
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
 modprobe br_netfilter
-sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.bridge.bridge-nf-call-iptables=1
-echo "br_netfilter" |tee /etc/modules-load.d/k8s.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" |tee /etc/sysctl.conf
-echo "net.ipv4.ip_forward=1" |tee /etc/sysctl.conf
-cat /etc/fstab | grep -v swap |tee temp.fstab
-cat temp.fstab |tee /etc/fstab
-rm temp.fstab
-swapoff -a
-mount --make-rshared /
-echo '#!/bin/sh' |tee /etc/local.d/sharemetrics.start
-echo "mount --make-rshared /" |tee -a  /etc/local.d/sharemetrics.start
-chmod +x /etc/local.d/sharemetrics.start
-rc-update add local
-uuidgen |sudo tee /etc/machine-id
-rc-update add docker
-rc-update add ntpd
-rc-update add kubelet default
-service kubelet restart
-service containerd restart
-/etc/init.d/ntpd start
-/etc/init.d/docker start
-ln -s /usr/libexec/cni/flannel-amd64 /usr/libexec/cni/flannel
-echo "net.bridge.bridge-nf-call-iptables=1" |tee -a /etc/sysctl.conf
-sysctl net.bridge.bridge-nf-call-iptables=1
-rc-update add crio default
-echo '
-[crio.runtime]
 
-# Overide defaults to not use systemd cgroups.
-conmon_cgroup = "pod"
-cgroup_manager = "cgroupfs"
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
 
-default_runtime = "crun"
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
 
-[crio.runtime.runtimes.crun]
-runtime_type = "oci"
-runtime_root = "/run/crun"
+apt install -y ipset ipvsadm
 
-[crio.network]
-network_dir = "/etc/cni/net.d/"
-plugin_dir = "/opt/cni/bin"
-' |tee /etc/crio/crio.conf
-echo 'runtime-endpoint: unix:///var/run/crio/crio.sock
-image-endpoint: unix:///var/run/crio/crio.sock
-timeout: 2
-' |tee /etc/crictl.yaml
+cat <<EOF | sudo tee /etc/modules-load.d/ipvs.conf
+ip_vs
+ip_vs_rr
+ip_vs_wrr
+ip_vs_sh
+nf_conntrack
+EOF
+
+modprobe ip_vs
+modprobe ip_vs_rr
+modprobe ip_vs_wrr
+modprobe ip_vs_sh
+modprobe nf_conntrack
+
+
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+bash -c "cat > /etc/docker/daemon.json <<EOF
+{
+  \"exec-opts\": [\"native.cgroupdriver=systemd\"]
+}
+EOF
+"
+
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.13/cri-dockerd_0.3.13.3-0.ubuntu-focal_amd64.deb
+dpkg -i cri-dockerd_0.3.13.3-0.ubuntu-focal_amd64.deb
+
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+systemctl restart docker
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
